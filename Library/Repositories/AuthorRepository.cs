c6fb5f5e630b243bae0f78b11067993e1dec76eb
vatchi <love.hermansson@live.se>
2015-10-23 16:49:18 +0200

    sggsg

---
+++ Library/Repositories/AuthorRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Library.Models;
+
+namespace Library.Repositories
+{
+    public class AuthorRepository : IRepository<Author, int>
+    {
+        LibraryContext _context;
+        string love;
+
+        public AuthorRepository (LibraryContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public void Add(Author item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Author item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Find(int id)
+        {
+            throw new NotImplementedException();        
+        }
+
+        public void Edit(Author item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Author> All()
+        {
+            return _context.Authors.ToList();
+        }
+    }
+}
